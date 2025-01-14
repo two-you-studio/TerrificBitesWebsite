482cb9d7a8b100e0c250f1480956f7d3a3b0e57e
two-you-studio <twoyoustudio@gmail.com>
2025-01-14 23:52:50 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
-import About from './pages/About.jsx'
-import PageNotFound from './pages/PageNotFound.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import About from './pages/About.jsx';
+import PageNotFound from './pages/PageNotFound.jsx';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      errorElement: <PageNotFound />,
+    },
+    {
+      path: '/about',
+      element: <About />,
+    },
+  ],
   {
-    path: '/', 
-    element: <App/>,
-    errorElement: <PageNotFound/>,
-  },
-  {
-    path: '/about',
-    element: <About/>,
+    basename: '/Terrific_Bites',
   }
-])
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
